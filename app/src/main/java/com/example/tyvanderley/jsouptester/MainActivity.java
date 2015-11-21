c2b1033bcf21aa18f5772314620023daaea5f0df
TBVanderley4545 <TBVanderley@gmail.com>
2015-11-21 13:04:03 -0500

    Added handling for no network connectivity.

--- app/src/main/java/com/example/tyvanderley/jsouptester/MainActivity.java
+++ app/src/main/java/com/example/tyvanderley/jsouptester/MainActivity.java
@@ -2,15 +2,24 @@ package com.example.tyvanderley.jsouptester;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
+
+    ValidationChecker vCheck = new ValidationChecker(this);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        new PullNameActivity(this, this).execute();
-        new PullPictureActivity(this,this).execute();
-    }
 
+        // If there is network connectivity, then pull data. If not, display an error toast.
+        if (vCheck.isNetworkAvailable()) {
+            new PullNameActivity(this, this).execute();
+            new PullPictureActivity(this,this).execute();
+        } else {
+            Toast.makeText(this, R.string.networkUnavailable, Toast.LENGTH_LONG).show();
+        }
+
+    }
 }
